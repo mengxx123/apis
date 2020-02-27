662e9ff704f8f790d4e646935736f1dfbaa7e244
ydying <1778679036@qq.com>
2020-02-27 18:00:40 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './index.css';
+
+export default function () {
+    return (
+        <div className={styles.normal}>
+            <div className="cssLib">
+
+            </div>
+            <ul className={styles.list}>
+                <li>
+                    To get started, edit <code>src/pages/index.js</code> and save to reload.
+        </li>
+                <li>
+                    <a href="https://umijs.org/guide/getting-started.html">Getting Started</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
